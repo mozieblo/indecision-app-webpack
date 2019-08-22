65473e9478a7b5282769977e7166ff91251a0141
mozieblo <m.ozieblo@wp.pl>
2019-08-22 22:55:15 +0200

    initial

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Header extends React.Component {
+    render () {
+        return (
+        <div>
+            <h1>Indecision App</h1>
+            <h2>Put your life in hands computer.</h2>
+        </div>
+        );
+    }
+}
+
+class Action extends React.Component {
+    render () {
+        return (
+        <div>
+            <button>What should I do?</button>
+        </div>
+        );
+    }
+}
+
+const jsx = (
+    <div>
+        <Header />
+        <Action />
+    </div>
+);
+
+
+ReactDOM.render(jsx, document.getElementById('app'));
