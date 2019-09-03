82cff203a7a46635f75a0c691351f9acee47cc78
mozieblo <m.ozieblo@wp.pl>
2019-09-03 18:05:01 +0200

    style

--- src/components/Options.js
+++ src/components/Options.js
@@ -5,20 +5,23 @@ import Option from './Option';
 const Options = (props) => {
     return (
         <div>
-            {props.options.length === 0 && <p>Please add an option to get started!</p>}
+            <div className="widget-header">
+                <h3 className="widget-header__title">Your Options</h3>
+                <button 
+                    className="button button--link"
+                    onClick={props.handleDeleteOptions}
+                >
+                Remove All
+                </button>
+            </div>
+            {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}             
             {props.options.map((option) => 
                 <Option 
                     key={option} 
                     optionText={option}
                     handleDeleteOption={props.handleDeleteOption}
                 />
-                )}
-            <button 
-                className="button button--link"
-                onClick={props.handleDeleteOptions}
-            >
-            Remove All
-            </button>
+            )}
         </div>    
         );
 };
